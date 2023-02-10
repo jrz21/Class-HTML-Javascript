487285ef6b96830116c3974a93bbeacf888e340c
jrz21 <89151058+jrz21@users.noreply.github.com>
2023-02-10 10:11:00 -0500

    Add files via upload

---
+++ const IVA = 21;.js
@@ -0,0 +1,23 @@
+//Decraracion de Variables v1 
+
+const IVA = 21;
+var a;
+let num = 2;
+
+const array = [];
+
+const obj = {
+
+    a : 1, b : 2,
+}
+
+/*Condicionales*/
+
+if (condition) {
+    
+} else {
+
+} while (condition) {
+
+}
+for (let index = 0; index < array.length; )
